aef65f9d5c333d8a38154dab9f54f15172d69234
Megan <79134357+tanmymegan@users.noreply.github.com>
2022-10-23 15:18:55 +0800

    when touch sensor is inactive break

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@ let distance = 0
 radio.setGroup(3)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     basic.showIcon(IconNames.Happy)
     radio.sendString("trolley " + ("" + control.deviceSerialNumber()))
     if (pins.digitalReadPin(DigitalPin.P2) == 1) {
         basic.showIcon(IconNames.Yes)
-        basic.pause(1000)
+        basic.pause(500)
         basic.clearScreen()
         while (pins.digitalReadPin(DigitalPin.P2) == 1) {
             pins.digitalWritePin(DigitalPin.P1, 1)
@@ -19,13 +18,11 @@ basic.forever(function on_forever() {
             } else {
                 pins.digitalWritePin(DigitalPin.P0, 0)
             }
-            
             if (pins.digitalReadPin(DigitalPin.P2) == 0) {
                 basic.showIcon(IconNames.No)
                 basic.pause(5000)
+                break;
             }
-            
         }
     }
-    
 })
